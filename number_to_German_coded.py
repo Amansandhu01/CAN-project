8919023b1e1af10a83f8de68ff4f6d6026345273
Jaspreet Kaur <jaspreet@MacBookPro.attlocal.net>
2024-12-05 23:54:01 -0800

    revised

--- number_to_German_coded.py
+++ number_to_German_coded.py
@@ -1,29 +1,28 @@
 import math as ma
-a = "10907"
+
 def num2GermanStyle():
-    ones=str(a[-1]) if a[-1] !=0 else None
-    tens=str(a[-2])
+    ones=int(a[-1]) 
+    tens=int(a[-2])
 
-    hun = str(a[-3]) if a[-3] !=0 and len(a) >=3 else None
+    hun = int(a[-3]) if a[-3] !=0 and len(a) >=3 else None
 
-    thou= str(a[-5:-3]) if len(a) >=4 else None
+    thou= int(a[-5:-3]) if len(a) >=4 else None
 
     m = [' ','Ten','Twenty','Thirty','Forty','Fifty','Sixty','Seventy','Eighty','Ninety']
     n = [' ','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
-    o = int(ones) % 10
-    h = int(hun)
-    t = int(thou) % 10 if thou is not None else None
+    o = ones % 10 if ones !=0 else None
+    t = (thou % 10) if thou is not None else None
     t1 = ma.floor(int(thou) / 10) if thou is not None else None
-    print(thou, {n[t]},{m[t1]},a[-4])
+   
+    p1 = f"{n[t]} {m[t1]} Thousand" if str(a[-4]) != '0' and thou is not None and len(a) > 4  else f"{m[t1]} Thousand" if str(a[-4]) == '0' and thou is not None and len(a) > 4 else f"{n[t]} Thousand" if thou is not None and len(a) == 4 else ''
+ 
+    p2 = f"{n[hun]} hundred" if str(a[-3]) != '0' else ""
+    p3 = f"{n[o]}" if o is not None else ""
+    p4 = f"and {m[int(tens)]}" if str(a[-2]) != '0' else ""
+    print(f"{p1} {p2} {p3}{p4}")
     
-    p1 = f"{n[t]} and {m[t1]} Thousand" if str(a[-4]) != '0' and thou is not None and len(a) > 4  else f"{m[t1]} Thousand" if str(a[-4]) == '0' and thou is not None and len(a) > 4 else f"{n[t]} Thousand" if thou is not None and len(a) == 4 else ''
-    print(locals())
-    p2 = f"{n[h]} hundred and " if str(a[-3]) != '0' else ''
-    p3 = f"and {m[int(tens)]}" if str(a[-2]) != '0' else ''
 
-    print(f"{p1} {p2}{n[int(ones)]} {p3}" )
-
-
+a = "90090"
 if len(a) >= 4:
     if __name__ == '__main__':
         num2GermanStyle()
